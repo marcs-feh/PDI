87ff258a86deefdbee3816beb75743fd63e53b6c
marcs-feh <marcosfehlauer@protonmail.com>
2023-10-04 20:53:03 -0300

    threshold

--- filter.py
+++ filter.py
@@ -1,21 +1,26 @@
 from typing import Any
 import numpy as np
 
-def apply_filter(img: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+CONVOLVE = 0
+CORRELATE = 1
+
+def apply_filter(img: np.ndarray, kernel: np.ndarray, mode: int = CONVOLVE) -> np.ndarray:
+    assert (mode == CONVOLVE) or (mode == CORRELATE)
+    f = convolve2D if mode == CONVOLVE else correlate2D
     chans = []
     if len(img.shape) > 2:
         chans = split_channels(img)
         for i in range(0, len(chans)):
-            chans[i] = convolve2D(chans[i], kernel).clip(0, 1.0)
+            chans[i] = f(chans[i], kernel).clip(0, 1.0)
         return join_channels(chans)
     else:
-        out = convolve2D(img, kernel).clip(0, 1.0)
+        out = f(img, kernel).clip(0, 1.0)
         return out
 
-def apply_filters(img: np.ndarray, kernels: list[np.ndarray]) -> list[np.ndarray]:
+def apply_filters(img: np.ndarray, kernels: list[np.ndarray], mode: str = CONVOLVE) -> list[np.ndarray]:
     results = []
     for k in kernels:
-        results.append(apply_filter(img, k))
+        results.append(apply_filter(img, k, mode=mode))
     return results
 
 def valid_kernel_shape(shape) -> bool:
@@ -32,7 +37,7 @@ def join_channels(chans: list[np.ndarray]) -> np.ndarray:
     return stacked
 
 # TODO: Improve perf, python `for` is way too slow
-def convolve2D(img: np.ndarray, kernel: np.ndarray, padding_val=0) -> np.ndarray:
+def correlate2D(img: np.ndarray, kernel: np.ndarray, padding_val=0) -> np.ndarray:
     assert valid_kernel_shape(kernel.shape)
 
     off = kernel.shape[0] // 2
@@ -48,4 +53,16 @@ def convolve2D(img: np.ndarray, kernel: np.ndarray, padding_val=0) -> np.ndarray
             out[row - off][col - off] = (kernel * slice).sum()
             
     return out
+
+def convolve2D(img: np.ndarray, kernel: np.ndarray, padding_val=0) -> np.ndarray:
+    return correlate2D(img, kernel[::-1], padding_val=padding_val)
+
+def invert(img: np.ndarray) -> np.ndarray:
+    return (-img) + 1.0
+
+def threshold(img: np.ndarray, t: float = 0.5) -> np.ndarray:
+    f = np.vectorize(lambda x: 0.0 if x < t else 1.0)
+
+    return f(img)
+
 
--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from filter import apply_filter, apply_filters
+from filter import *
 from img_file import *
 from color import *
 from kernels import *
-from noise import gaussian_noise
+from noise import *
+from time import sleep
 
 def main():
     img = img_read('in.png')
     print(f'loaded {np.prod(img.shape) * 4} bytes')
     out = img
-    gs = grayscale_human_weighted(img)
-    # out *= rgb_from_grayscale(gaussian_noise(gs, 0.1))
-    # out = apply_filter(out, uniform_blur(3))
-    he, we = apply_filters(gs, sobel_edge_detect())
-    out -= rgb_from_grayscale(he + we)
-    img_write('out.png', out)
 
-    return
+    N = 14
+    for i in range(0, int(N)):
+        n = (1.0/N) * i
+        out *= threshold(img, n)
+    img_write('out.png', out)
 
 if __name__ == '__main__': main()
 
