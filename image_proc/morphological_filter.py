5edd6fc32fe578547543de4752637b6258383d9c
marcs-feh <marcosfehlauer@protonmail.com>
2023-11-26 18:18:10 -0300

    add dilate and erode

--- image_proc/morphological_filter.py
+++ image_proc/morphological_filter.py
@@ -1,1 +1,43 @@
 # TODO: everything
+from functools import reduce
+import numpy as np
+
+from image_proc.spatial_filter import valid_kernel_shape
+
+MORPH_MODES = {
+    'erode',
+    'dilate',
+}
+
+def morph(mode:str, img: np.ndarray, kernel: np.ndarray, padding_val: float = 0):
+    assert mode in MORPH_MODES
+    assert valid_kernel_shape(kernel.shape)
+    off = kernel.shape[0] // 2
+
+    mask = kernel.flatten()
+    padded = np.pad(img, (off, off), 'constant', constant_values=padding_val)
+
+    height, width = padded.shape[0], padded.shape[1]
+    out = np.ndarray(img.shape, dtype=np.float32)
+
+    for row in range(off, height - off):
+        for col in  range(off, width - off):
+            slice = padded[row-off:row+off+1, col-off:col+off+1].flatten()
+            pixels = []
+            for i in range(0, len(slice)):
+                if mask[i] != 0: pixels.append(slice[i])
+
+            pixels = np.array(pixels, dtype=np.float32)
+
+            ok = False
+            count = (pixels > 0).sum()
+            if mode == 'erode':
+                n = len(pixels)
+                ok = count >= n
+            elif mode == 'dilate':
+                ok = count > 0
+
+            out[row - off][col - off] = 1.0 if ok else 0.0
+
+    return out
+
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+
+from numpy.ma import negative
 import image_proc as ip
 import numpy as np
 
-# TODO:
-# Filtragem Espacial:
-# OpenCV Canny (?????)
+from image_proc.spatial_filter import gaussian_blur
+
+
+k = np.array(
+    [
+    [1,0,0,0,0],
+    [1,1,1,0,0],
+    [0,1,1,1,0],
+    [0,0,1,1,1],
+    [0,0,0,0,1],
+    ],
+    dtype=np.float32
+); 
 
 def main():
     img = ip.img_read('in.png')
     print(f'loaded {np.prod(img.shape) * 4} bytes')
 
     out = ip.grayscale_weighted(img)
-    out = ip.roberts_edge_detect(out)
+    thresh = ip.threshold(out, 0.2, 1.0)
+    e = ip.morph('erode', thresh, k)
+    d = ip.morph('dilate', thresh, k)
+    out = out - 0.15 * gaussian_blur(d - e)
+    # out = ip.roberts_edge_detect(out)
 
     ip.img_write('out.png', out)
 
